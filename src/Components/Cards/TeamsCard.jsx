8dc83eb84c5e23b12302ae1f3e342c17d9d6e8f5
Ameer Mavia <maviaking97@gmail.com>
2025-11-21 03:56:34 +0500

    Changes

--- src/Components/Cards/TeamsCard.jsx
+++ src/Components/Cards/TeamsCard.jsx
@@ -1,23 +1,29 @@
+"use client"
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const TeamsCard = ({title , img , desc , designation}) => {
+    const [open, setOpen] = useState(false);
+
+
     return (
-        <div data-aos="zoom-in-up" className="group relative cursor-pointer">
+        <div data-aos="zoom-in-up" onClick={() => setOpen(!open)} className="group relative cursor-pointer">
 
             <div
+            
                 className="
           relative rounded-xl overflow-hidden 
           bg-[#0B0F1A]
           transition-all duration-[1000ms] ease-[cubic-bezier(0.6,0.03,0.28,0.98)]
+          
         "
             >
 
                 {/* Text Block */}
-                <div style={{backdropFilter: "blur(10px)"}} className="absolute left-4 right-4 bottom-[-100%] py-5 rounded-[10px]  z-50 bg-[#ffffff2c] w-[90%]  
-                  transition-all duration-700 ease-out 
-                  group-hover:bottom-4">
+                <div style={{backdropFilter: "blur(10px)"}} className={`absolute left-4 right-4  py-5 rounded-[10px]  z-50 bg-[#ffffff2c] w-[90%]  
+                  transition-all duration-700 ease-out   ${open ? "bottom-4" : "bottom-[-100%]"}
+                  group-hover:bottom-4`}>
                     <div className="transition-all duration-1000 flex flex-col gap-2 sm:gap-1 z-40 relative items-center  text-center px-6">
                         
                         <h4 className="text-white text-[22px] sm:text-[26px] leading-[30px] font-Manrope font-semibold">{title } </h4>
